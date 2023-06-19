9dbf862b551ad77f8f50c2fb9e1f539b60888ac1
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2023-06-19 12:47:27 +0000

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/astrainingbusiness',
+mongoose.connect('mongodb+srv://jeanakoupaul:jupy0500908420@cluster0.t2vaevc.mongodb.net/',
 (err)=>{
     if(!err){console.log('la connexion MongoDB a demarré avec success ')}
   else{ console.log(`Une erreur s'est produite  dans la base de donnée:` +err)}
