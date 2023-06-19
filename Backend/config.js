7a861b9935dbc7fbabc016c6f4af9885fb63fb62
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2023-06-19 13:36:19 +0000

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -2,7 +2,6 @@ const mongoose = require ('mongoose')
 
 mongoose.connect('mongodb+srv://jeanakoupaul:jupy0500908420@cluster0.t2vaevc.mongodb.net/',{
       useNewUrlParser: true,
-      useCreateIndex: true,
       useUnifiedTopology: true
     }).then(() => console.log( 'Database Connected' ))
      .catch(err => console.log( err ));
